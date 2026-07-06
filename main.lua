95f1b33cffabe51de89751e77855c1cc1d30ecbf
florane <qwerty.qa912@gmail.com>
2026-07-06 08:01:51 +0700

    replaced json with csv and added text

--- main.lua
+++ main.lua
@@ -10,15 +10,19 @@ local storage = {}
 
 MenuCallbackHandler[modId.."_save"] = function(self)
 	local file = io.open(SavePath..modId..".txt","w")
-	if file then
-		file:write(json.encode(storage))
+	if file then -- csv parsing bc json lib cannot work with spread tables
+		for k,v in pairs(storage) do
+			file:write(tostring(k)..",")
+			for _k,_v in pairs(v) do file:write(_k..",".._v..",") end
+			file:write("\n")
+		end
 		file:close()
 	end
 end
 
 local characterIndex = {} --i love preemptive optimization!!!
 for _,v in ipairs(characterIds) do
-	characterIndex["menu_"..v] = true
+	characterIndex["menu_"..v] = v
 
 	MenuCallbackHandler[modId.."_"..v.."_set"] = function (self, item)
 		if not managers.multi_profile then return end
@@ -31,7 +35,13 @@ end
 Hooks:Add("MenuManagerBuildCustomMenus","MenuManagerBuildCustomMenus_profile_renames",function(menu_manager, nodes)
 	local file = io.open(SavePath..modId..".txt","r")
 	if file then
-		storage = json.decode(file:read("*all")) or {}
+		for line in file:lines() do -- csv parsing bc json lib cannot work with spread tables
+			local buffer = {}
+			for v in string.gmatch(line,"(.-),") do buffer[#buffer+1] = v end
+			buffer[1] = tonumber(buffer[1])
+			storage[buffer[1]] = {}
+			for i=2,#buffer,2 do storage[buffer[1]][buffer[i]] = buffer[i+1] end
+		end
 		file:close()
 	end
 
@@ -53,10 +63,39 @@ Hooks:Add("MenuManagerBuildCustomMenus","MenuManagerBuildCustomMenus_profile_ren
 end)
 
 Hooks:PostHook(LocalizationManager,"text",modId.."_text",function(self, string_id)
-	if not characterIndex["menu_"..string_id] then return Hooks:GetReturn() end
+	if not characterIndex[string_id] then return Hooks:GetReturn() end
 	if not managers.multi_profile then return Hooks:GetReturn() end
 	local current_profile = managers.multi_profile._global._current_profile
 	if not storage[current_profile] then return Hooks:GetReturn() end
-	if not storage[current_profile]["menu_"..string_id] then return Hooks:GetReturn() end
-	return storage[current_profile]["menu_"..string_id]
+	if not storage[current_profile][characterIndex[string_id]] then return Hooks:GetReturn() end
+	return storage[current_profile][characterIndex[string_id]]
+end)
+
+Hooks:Add("LocalizationManagerPostInit",modId.."LocalizationManagerPostInit",function()
+	LocalizationManager:add_localized_strings({
+		["menu_"..modId] = "Profile-tied Character Renames",
+		["menu_"..modId.."_desc"] = "Settings with inputs to rename every character in currently selected profile",
+		[modId.."russian_desc"] = "Dallas",
+		[modId.."german_desc"] = "Wolf",
+		[modId.."spanish_desc"] = "Chains",
+		[modId.."american_desc"] = "Houston",
+		[modId.."jowi_desc"] = "John Wick",
+		[modId.."old_hoxton_desc"] = "Hoxton",
+		[modId.."female_1_desc"] = "Clover",
+		[modId.."dragan_desc"] = "Dragan",
+		[modId.."jacket_desc"] = "Jacket",
+		[modId.."bonnie_desc"] = "Bonnie",
+		[modId.."sokol_desc"] = "Sokol",
+		[modId.."dragon_desc"] = "Jiro",
+		[modId.."bodhi_desc"] = "Bodhi",
+		[modId.."jimmy_desc"] = "Jimmy",
+		[modId.."sydney_desc"] = "Sydney",
+		[modId.."wild_desc"] = "Rust",
+		[modId.."chico_desc"] = "Scarface",
+		[modId.."max_desc"] = "Sangres",
+		[modId.."joy_desc"] = "Joy",
+		[modId.."myh_desc"] = "Duke",
+		[modId.."ecp_male_desc"] = "I do not respect your choice of character",
+		[modId.."ecp_female_desc"] = "I do not respect your choice of character"
+	})
 end)
